76ad994e778768adda86ff24a2a136fd179c666a
HaileyRags <haileyragsdale2002@gmail.com>
2026-05-03 22:21:42 -0500

    Add initial project setup with Postman resources, environment configurations, and Express server

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'vitaltrack',
+    password: 'LudySmokey20!2',
+    port: 5423,
+});
+
+app.get('/api/test-db', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json({ message: "PostgreSQL Connect!", time: result.rows[0] });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Database connect failed.");
+    }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`VitalTrack API running on port ${PORT}`));
